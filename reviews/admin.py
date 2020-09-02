5cfd525c7a21b572301005330921fcd0084a88eb
Kimminsu <alstn2468_@naver.com>
2020-09-02 14:24:45 +0900

    Add review model and register admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,1 +1,20 @@
 from django.contrib import admin
+from reviews.models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "created_by",
+        "text",
+        "rating",
+        "movie",
+        "book",
+    )
+
+    list_filter = (
+        "created_by__username",
+        "movie",
+        "book",
+    )
+
--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from core.models import AbstractTimeStamp
 
 """
 Here are the models you have to create:
@@ -9,3 +10,26 @@ Here are the models you have to create:
   book (ForeignKey => movies.Movie, null,blank)
   rating
 """
+
+
+class Review(AbstractTimeStamp):
+    created_by = models.ForeignKey(
+        "users.User", related_name="user", on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    rating = models.FloatField(default=0)
+    movie = models.ForeignKey(
+        "movies.Movie",
+        related_name="movie",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    book = models.ForeignKey(
+        "books.Book",
+        related_name="book",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
