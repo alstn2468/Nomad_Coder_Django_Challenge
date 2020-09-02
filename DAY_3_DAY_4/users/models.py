b0d0cffe1cbcdb0b72d041e4fb210962d0a4de42
Kimminsu <alstn2468_@naver.com>
2020-09-02 11:18:39 +0900

    Fix user model fav genre field to fk

--- DAY_3_DAY_4/users/models.py
+++ DAY_3_DAY_4/users/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from categories.models import Category
+
+
+def set_cateogry_fk_model():
+    return Category.objects.all()[:1].get()
 
 
 class User(AbstractUser):
@@ -12,20 +17,19 @@ class User(AbstractUser):
     LANG_KR = "korean"
     LANG_CHOICES = ((LANG_EN, "English"), (LANG_KR, "Korean"))
 
-    GENRE_ACTION = "action"
-    GENRE_COMEDY = "comedy"
-    GENRE_THRILLER = "thriller"
-    GENRE_HISTORY = "history"
-
-    GENRE_CHOICES = (
-        (GENRE_ACTION, "Action"),
-        (GENRE_COMEDY, "Comedy"),
-        (GENRE_THRILLER, "Thriller"),
-        (GENRE_HISTORY, "History"),
+    bio = models.TextField()
+    preference = models.CharField(
+        max_length=20, choices=PREF_CHOICES, default=PREF_MOVIES
+    )
+    language = models.CharField(max_length=20, choices=LANG_CHOICES, default=LANG_EN)
+    fav_book_genre = models.ForeignKey(
+        "categories.Category",
+        related_name="fav_book_genre",
+        on_delete=models.SET(set_cateogry_fk_model),
+    )
+    fav_movie_genre = models.ForeignKey(
+        "categories.Category",
+        related_name="fav_movie_genre",
+        on_delete=models.SET(set_cateogry_fk_model),
     )
 
-    bio = models.TextField()
-    preference = models.CharField(max_length=20, choices=PREF_CHOICES, default=PREF_MOVIES)
-    language = models.CharField(max_length=20, choices=LANG_CHOICES, default=LANG_EN)
-    fav_book_genre = models.CharField(max_length=20, choices=GENRE_CHOICES, default=GENRE_HISTORY)
-    fav_movie_genre = models.CharField(max_length=20, choices=GENRE_CHOICES, default=GENRE_HISTORY)
