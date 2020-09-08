92b1c4878e8fb13647086412b539add8246392a4
Kimminsu <alstn2468_@naver.com>
2020-09-08 09:42:11 +0900

    Fix seed_favlists logic

--- favs/management/commands/seed_favlists.py
+++ favs/management/commands/seed_favlists.py
@@ -1,53 +1,47 @@
 from django.contrib.admin.utils import flatten
-from core.management.commands.custom_command import CustomCommand
+from django.db import IntegrityError
+from copy import deepcopy
 from django_seed import Seed
 from random import randint, choice
 from movies.models import Movie
 from books.models import Book
 from users.models import User
 from favs.models import FavList
+from core.management.commands.custom_command import CustomCommand
 
 
 class Command(CustomCommand):
     help = "Automatically create fav lists"
 
-    def add_arguments(self, parser):
-        parser.add_argument("--total", default=1, help="Number of fav lists to create")
-
     def handle(self, *args, **options):
         try:
-            total = int(options.get("total"))
-
             self.stdout.write(self.style.SUCCESS("■ START CREATE FAV LISTS"))
 
             users = User.objects.all()
             books = Book.objects.all()
             movies = Movie.objects.all()
 
-            seeder = Seed.seeder()
-            seeder.add_entity(
-                FavList,
-                total,
-                {"created_by": lambda x: choice(users)},
-            )
-
-            clean_pk_list = flatten(list(seeder.execute().values()))
-
-            for idx, pk in enumerate(clean_pk_list):
+            for idx, user in enumerate(users):
                 self.progress_bar(
                     idx + 1,
-                    len(clean_pk_list),
+                    len(users),
                     prefix="■ PROGRESS",
                     suffix="Complete",
                     length=40,
                 )
 
-                fav_list_model = FavList.objects.get(pk=pk)
-                books = books[randint(0, 5) : randint(6, len(books))]
-                movies = movies[randint(0, 5) : randint(6, len(movies))]
+                try:
+                    fav_list = FavList.objects.create(created_by=user)
 
-                fav_list_model.books.add(*books)
-                fav_list_model.movies.add(*movies)
+                    selected_books = books[randint(0, 5) : randint(6, len(books))]
+                    selected_movies = movies[randint(0, 5) : randint(6, len(movies))]
+
+                    fav_list.books.add(*selected_books)
+                    fav_list.movies.add(*selected_movies)
+
+                    fav_list.save()
+                except IntegrityError:
+                    continue
 
             self.stdout.write(self.style.SUCCESS("■ SUCCESS CREATE ALL FAV LISTS!"))
 
