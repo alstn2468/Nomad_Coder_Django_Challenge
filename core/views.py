7ec7c5fb363ad0417783637ad4a9595b9b1731c7
Kimminsu <alstn2468_@naver.com>
2020-09-14 10:07:13 +0900

    Remove useless import

--- core/views.py
+++ core/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator, EmptyPage
 from movies.models import Movie
 from books.models import Book
 from people.models import Person
